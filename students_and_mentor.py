373c5dc2deec57acbea403912e78c991826f97e9
Sergei Rut <ioga14@mail.ru>
2021-07-03 14:02:36 +0300

    Initial commit

---
+++ students_and_mentor.py
@@ -0,0 +1,60 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        self.grades_lecturer = {}
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Lecturer(Mentor):
+    def rate_lecturer(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades_lecturer:
+                student.grades_lecturer[course] += [grade]
+            else:
+                student.grades_lecturer[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_reviewer = Reviewer('Nik', 'Kin')
+cool_reviewer.courses_attached += ['Python']
+
+cool_lecturer = Lecturer('Nik1', 'Kin1')
+cool_lecturer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+cool_lecturer.rate_lecturer(best_student, 'Python', 10)
+
+print(best_student.grades)
+print(best_student.grades_lecturer)
